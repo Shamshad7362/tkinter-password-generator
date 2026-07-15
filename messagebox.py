6d330a7d697e5c5aee32a8b39ccefde4bdc25ec0
Shamshad Ansari <ansarisamsad008@gmail.com>
2026-07-15 18:29:15 +0400

    going back

--- messagebox.py
+++ messagebox.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 
 window = tk.Tk()
-window.title("Password Manager")
+window.title("Tkinter Password Manager")
 window.geometry("500x500")
 
 value=messagebox.showinfo("Password Manager", "Password Manager has been created")
